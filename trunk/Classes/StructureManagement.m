5e91b58bccc2dd6afe7c65ecac5b373778103866
bryan.taylor@st.com <bryan.taylor@st.com@735079b6-d188-99ff-e622-f490c25b29e4>
2013-01-02 11:34:33 +0000

    struct2dataset function added.

--- trunk/Classes/StructureManagement.m
+++ trunk/Classes/StructureManagement.m
@@ -11,5 +11,34 @@ classdef StructureManagement < handle
         function Table = struct2Table(obj,struct)
             Table = [rot90(fieldnames(struct),3);fliplr(rot90(squeeze(struct2cell(struct))))];
         end
+        function [DATASET] = struct2DATASET(obj,struct)
+           
+            %%
+            ProgramNames = fieldnames(struct);
+            x = size(ProgramNames,1);
+            count = 1;
+            for i = 1:x
+                ProgramName = ProgramNames{i};
+                ResultNames = fieldnames(struct.(ProgramName));
+                y = size(ResultNames,1);
+                for j = 1:y
+                    Program{count,1} = ProgramName;
+                    Result{count,1} = ResultNames{j};
+                    AgentName{count,1} = struct.(ProgramName).(ResultNames{j}).AgentName;
+                    Started(count,1) = struct.(ProgramName).(ResultNames{j}).Started;
+                    Complete(count,1) = struct.(ProgramName).(ResultNames{j}).Complete;
+                    temp = struct.(ProgramName).(ResultNames{j}).TimeOfLastPulse;
+                    if ischar(temp)
+                        temp = NaN;                      
+                    end
+                    TimeOfLastPulse(count,1) = temp;
+                    count = count + 1;
+                end
+            end
+            %%
+            DATASET = dataset(Program,Result,Started,Complete,TimeOfLastPulse,AgentName);
+            x = size(DATASET,1);
+            disp(['Number Of Tasks for this PC: ',num2str(x)])
+        end
     end
 end
