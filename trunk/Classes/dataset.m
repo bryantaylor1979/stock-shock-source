6c4ca01fcfd658afea36d48d6657862d931c678a
bryan.taylor465@gmail.com <bryan.taylor465@gmail.com@735079b6-d188-99ff-e622-f490c25b29e4>
2014-11-11 09:42:59 +0000

    url_download running commit 2

---
+++ trunk/Classes/dataset.m
@@ -0,0 +1,25 @@
+classdef dataset < handle & ...
+                   dynamicprops
+    properties
+    end
+    methods (Static)
+        function Example(obj)
+            %%
+            close all
+            clear classes
+            obj = dataset({[1,2,3,4],'Num'},{{'a','b','c'},'String'})
+        end
+    end
+    methods
+        function obj = dataset(varargin)
+            x = size(varargin,2);
+            for i = 1:x
+                set = varargin{i};
+                VarName = set{2};
+                Values = set{1};
+                obj.addprop(VarName)
+                obj.(VarName) = Values
+            end
+        end
+    end
+end
