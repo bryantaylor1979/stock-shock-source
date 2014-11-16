666d40453d61ab54c5726cd2f447c6b2d844a0ad
bryan.taylor465@gmail.com <bryan.taylor465@gmail.com@735079b6-d188-99ff-e622-f490c25b29e4>
2014-11-16 20:40:24 +0000

    try to get british bulls download working

--- WebDecoder/Macros/BB_HIST_Decode.m
+++ WebDecoder/Macros/BB_HIST_Decode.m
@@ -5,12 +5,4 @@ jenkinsRoot = '/var/lib/jenkins/jobs/';
 %% Paths
 workspace = [jenkinsRoot,jobName,'/workspace'];
 Symbols = obj.GetSavedSymbolsFromPath([workspace,'/URL_Download/Results']);
-CE_DataSet = obj.BB_Hist.Hist_URL2Data_Sync(Symbols,floor(now));
-
-%%
-try
-save([workspace,'/WebDecoder/Results/DecodedDATASET.mat'],'CE_DataSet')
-catch
-mkdir([workspace,'/WebDecoder/Results/']) 
-save([workspace,'/WebDecoder/Results/DecodedDATASET.mat'],'CE_DataSet')
-end
+obj.BB_Hist.URL2Table_ALL(Symbols,[workspace,'/URL_Download/Results'],[workspace,'/WebDecoder/Results/']);
